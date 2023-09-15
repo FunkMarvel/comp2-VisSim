975f666413f0e2092d4d45a4724af87b2ae084fc
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-09-15 08:17:36 +0200

    Update BallPhysics.cs

--- Assets/Scripts/BallPhysics.cs
+++ Assets/Scripts/BallPhysics.cs
@@ -3,8 +3,8 @@
 // //FileName: BallPhysics.cs
 // //FileType: Visual C# Source file
 // //Author : Anders P. Åsbø
-// //Created On : 13/09/2023
-// //Last Modified On : 13/09/2023
+// //Created On : 14/09/2023
+// //Last Modified On : 14/09/2023
 // //Copy Rights : Anders P. Åsbø
 // //Description :
 // //////////////////////////////////////////////////////////////////////////
@@ -21,12 +21,12 @@ public class BallPhysics : MonoBehaviour
     private float mass = 1;
 
     [SerializeField] [Min(0)] private float radius = 1;
-    [SerializeField] [Min(0)] private float rollingResistance = 0;
+    [SerializeField] [Min(0)] private float rollingResistance;
     [SerializeField] [Range(0, 1)] private float bounciness = 1;
 
     private bool _hasSurfaceRef;
-    private TriangleSurface _triangleSurface;
     private bool _outOfBounds;
+    private TriangleSurface _triangleSurface;
 
     private Vector3 _velocity = Vector3.zero;
 
@@ -42,41 +42,38 @@ public class BallPhysics : MonoBehaviour
 
         _triangleSurface = triangleSurfaceRef.GetComponent<TriangleSurface>();
         _hasSurfaceRef = _triangleSurface != null;
-        
-        if (!_hasSurfaceRef)
-        {
-            Debug.LogWarning($"{gameObject.name} has no reference to surface!");
-        }
+
+        if (!_hasSurfaceRef) Debug.LogWarning($"{gameObject.name} has no reference to surface!");
     }
 
     private void FixedUpdate()
     {
-        Transform transform1 = transform;
+        var transform1 = transform;
         var position = transform1.position;
-        
-        float rollingCoefficient = _velocity.magnitude > 1e-15f ? rollingResistance : 0;
-        
-        Vector3 netForce = Physics.gravity * mass;
-        
+
+        var rollingCoefficient = _velocity.magnitude > 1e-15f ? rollingResistance : 0;
+
+        var netForce = Physics.gravity * mass;
+
         if (_hasSurfaceRef)
         {
-            
-            
             var hit = _triangleSurface.ProjectOntoSurface(position);
-            Vector3 distVec = position - hit.Point;
-            float dist = distVec.magnitude;
-            
+            var distVec = position - hit.Point;
+            var dist = distVec.magnitude;
+
             if (dist <= radius)
             {
-                if (Mathf.Abs(dist - radius) > 0.5f*radius) transform.position += (radius - dist) * distVec.normalized;
+                if (Mathf.Abs(dist - radius) > 0.5f * radius)
+                    transform.position += (radius - dist) * distVec.normalized;
                 // transform.position += (radius - dist) * distVec.normalized;
-                
-                Vector3 parallelUnitVector = Vector3.ProjectOnPlane(_velocity, hit.HitNormal).normalized;
-                _velocity = -bounciness*Vector3.Dot(_velocity, hit.HitNormal)*hit.HitNormal + Vector3.ProjectOnPlane(_velocity, hit.HitNormal);
-                
-                float normalForceMagnitude = Vector3.Dot(netForce, hit.HitNormal);
-                
-                netForce -= (hit.HitNormal - rollingCoefficient*parallelUnitVector)*normalForceMagnitude;
+
+                var parallelUnitVector = Vector3.ProjectOnPlane(_velocity, hit.HitNormal).normalized;
+                _velocity = -bounciness * Vector3.Dot(_velocity, hit.HitNormal) * hit.HitNormal +
+                            Vector3.ProjectOnPlane(_velocity, hit.HitNormal);
+
+                var normalForceMagnitude = Vector3.Dot(netForce, hit.HitNormal);
+
+                netForce -= (hit.HitNormal - rollingCoefficient * parallelUnitVector) * normalForceMagnitude;
             }
 
             if (!_outOfBounds && hit.HitNormal.magnitude < 1e-15f)
@@ -86,11 +83,11 @@ public class BallPhysics : MonoBehaviour
             }
         }
 
-        Vector3 acceleration = netForce / mass;
+        var acceleration = netForce / mass;
         _velocity += acceleration * Time.fixedDeltaTime;
-        
-        transform1.Translate(_velocity*Time.fixedDeltaTime);
-        
+
+        transform1.Translate(_velocity * Time.fixedDeltaTime);
+
         Debug.Log($"Position: {position} | " +
                   $"Velocity {_velocity} | " +
                   $"Acceleration {acceleration}");
