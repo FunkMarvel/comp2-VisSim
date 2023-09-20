3145980a77a0ffcdae7e5dbf6527a9cb9cfdc1d6
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-09-20 18:05:11 +0200

    Create Contact.cs

---
+++ Assets/Scripts/Contact.cs
@@ -0,0 +1,35 @@
+﻿// //////////////////////////////////////////////////////////////////////////
+// //////////////////////////////
+// //FileName: Contact.cs
+// //FileType: Visual C# Source file
+// //Author : Anders P. Åsbø
+// //Created On : 20/09/2023
+// //Last Modified On : 20/09/2023
+// //Copy Rights : Anders P. Åsbø
+// //Description : Struct for handling collision data.
+// //////////////////////////////////////////////////////////////////////////
+// //////////////////////////////
+
+using UnityEngine;
+
+/// <summary>
+///     Struct containing read-only data for collision contacts.
+/// </summary>
+public struct Contact
+{
+    public Contact(Vector3 point, Vector3 hitNormal)
+    {
+        Point = point;
+        HitNormal = hitNormal;
+    }
+
+    /// <summary>
+    ///     Read-only Location of collision contact in world-space.
+    /// </summary>
+    public Vector3 Point { get; }
+
+    /// <summary>
+    ///     Read-only unit normal at contact-point.
+    /// </summary>
+    public Vector3 HitNormal { get; }
+}
