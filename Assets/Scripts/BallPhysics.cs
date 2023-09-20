5dcadad163939fdd97d498c79febae19628dd598
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-09-20 18:05:14 +0200

    Update BallPhysics.cs

--- Assets/Scripts/BallPhysics.cs
+++ Assets/Scripts/BallPhysics.cs
@@ -4,36 +4,45 @@
 // //FileType: Visual C# Source file
 // //Author : Anders P. Åsbø
 // //Created On : 14/09/2023
-// //Last Modified On : 14/09/2023
+// //Last Modified On : 20/09/2023
 // //Copy Rights : Anders P. Åsbø
 // //Description :
 // //////////////////////////////////////////////////////////////////////////
 // //////////////////////////////
 
-using System;
 using UnityEngine;
 
 public class BallPhysics : MonoBehaviour
 {
     [Header("References")] [SerializeField]
-    private GameObject triangleSurfaceRef;
+    private GameObject triangleSurfaceRef; // reference triangle surface, editable in engine.
 
     [Header("Physical properties")] [SerializeField] [Min(1e-6f)]
-    private float mass = 1;
+    private float mass = 1; // mass of ball, editable in engine.
 
-    [SerializeField] [Min(0)] private float radius = 1;
-    [SerializeField] [Min(0)] private float rollingResistance;
-    [SerializeField] [Range(0, 1)] private float bounciness = 1;
+    [SerializeField] [Min(0)] private float radius = 1; // radius of ball, editable in engine.
+    [SerializeField] [Min(0)] private float rollingResistance; // friction, editable in engine.
+    [SerializeField] [Range(0, 1)] private float bounciness = 1; // bounciness, editable in engine.
 
+    // bools for reference handling:
     private bool _hasSurfaceRef;
     private bool _outOfBounds;
-    private TriangleSurface _triangleSurface;
 
-    private Vector3 _velocity = Vector3.zero;
+    // internal variable for drawing contact point:
     private Vector3 _prevContact = Vector3.zero;
 
+    // reference to instance of triangle surface:
+    private TriangleSurface _triangleSurface;
+
+    // physics velocity:
+    private Vector3 _velocity = Vector3.zero;
+
+    /// <summary>
+    ///     Setup before first frame.
+    /// </summary>
     private void Awake()
     {
+        // makes sure surface reference is set in engine editor:
         _hasSurfaceRef = triangleSurfaceRef != null;
 
         if (!_hasSurfaceRef)
@@ -42,60 +51,81 @@ public class BallPhysics : MonoBehaviour
             return;
         }
 
+        // retrieves instance of triangleSurface:
         _triangleSurface = triangleSurfaceRef.GetComponent<TriangleSurface>();
         _hasSurfaceRef = _triangleSurface != null;
 
         if (!_hasSurfaceRef) Debug.LogWarning($"{gameObject.name} has no reference to surface!");
     }
 
+    /// <summary>
+    ///     Physics update loop.
+    /// </summary>
     private void FixedUpdate()
     {
+        // caching transform, and position to avoid access overhead:
         var transform1 = transform;
         var position = transform1.position;
 
+        // sets rolling resistance to 0 when not in motion:
         var rollingCoefficient = _velocity.magnitude > 1e-15f ? rollingResistance : 0;
 
+        // gravity force, Physics.gravity is the acceleration-vector [0, -9.81, 0]:
         var netForce = Physics.gravity * mass;
 
         if (_hasSurfaceRef)
         {
-            var hit = _triangleSurface.ProjectOntoSurface(position);
-            _prevContact = hit.Point;
-            
+            // get current step and next step contact points:
+            var hit = _triangleSurface.GetCollision(position);
+            var nextHit = _triangleSurface.GetCollision(position + _velocity * Time.fixedDeltaTime);
+            _prevContact = hit.Point; // store contact point for debug drawing.
+
             var distVec = position - hit.Point;
             var dist = distVec.magnitude;
 
-            if (dist <= radius)
+            if (dist <= radius) // check if actually colliding
             {
-                
-                transform.position = hit.Point + radius * hit.HitNormal;
+                // correct position to avoid clipping:
+                position = hit.Point + radius * hit.HitNormal;
 
-                var parallelUnitVector = Vector3.ProjectOnPlane(_velocity, hit.HitNormal).normalized;
-                _velocity = -bounciness * Vector3.Dot(_velocity, hit.HitNormal) * hit.HitNormal +
-                            Vector3.ProjectOnPlane(_velocity, hit.HitNormal);
+                var parallelVelocity = Vector3.ProjectOnPlane(_velocity, hit.HitNormal);
 
+                var reflectNorm = (hit.HitNormal + nextHit.HitNormal).normalized;
+                var normalChange = Vector3.Cross(hit.HitNormal, nextHit.HitNormal).sqrMagnitude;
+
+                if (normalChange <= 0f) // reflect velocity when switching triangle:
+                    _velocity = -bounciness * Vector3.Dot(_velocity, hit.HitNormal) * hit.HitNormal + parallelVelocity;
+                else // bouncing when not switching triangle:
+                    _velocity -= 2 * Vector3.Dot(_velocity, reflectNorm) * reflectNorm;
+
+                // add normal-force:
                 var normalForceMagnitude = Vector3.Dot(netForce, hit.HitNormal);
-
-                netForce -= (hit.HitNormal - rollingCoefficient * parallelUnitVector) * normalForceMagnitude;
+                netForce -= (hit.HitNormal - rollingCoefficient * parallelVelocity.normalized) * normalForceMagnitude;
             }
 
-            if (!_outOfBounds && hit.HitNormal.magnitude < 1e-15f)
+            // remove physics-script from ball if out of bounds:
+            if (!_outOfBounds && Mathf.Approximately(hit.HitNormal.sqrMagnitude, 0f))
             {
                 _outOfBounds = true;
                 Destroy(this);
             }
         }
 
+        // integrate position with Forward-Euler:
         var acceleration = netForce / mass;
         _velocity += acceleration * Time.fixedDeltaTime;
 
         transform1.Translate(_velocity * Time.fixedDeltaTime);
 
+        // log position:
         Debug.Log($"Position: {position} | " +
                   $"Velocity {_velocity} | " +
                   $"Acceleration {acceleration}");
     }
 
+    /// <summary>
+    ///     Draw projected contact point in editor-view:
+    /// </summary>
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
