fdcd7236bac45ec2308c3f028860b88c0f22b6ce
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-10-25 18:16:38 +0200

    Create PointCloud.cs

---
+++ Assets/Scripts/PointCloud.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Globalization;
+using UnityEngine;
+
+public class PointCloud : MonoBehaviour
+{
+    [SerializeField] private TextAsset vertexFile;
+    [SerializeField] private Vector3 offset;
+
+    private Vector3[] _vertices;
+    private ComputeBuffer _pointBuffer;
+    
+
+    private void OnEnable()
+    {
+        ReadVertexData();
+        _pointBuffer = new ComputeBuffer(_vertices.Length, 3 * 4);
+        _pointBuffer.SetData(_vertices);
+    }
+
+    private void OnDisable()
+    {
+        _pointBuffer.Release();
+        _pointBuffer = null;
+    }
+
+    /// <summary>
+    ///     Read vertex-data from file.
+    /// </summary>
+    private void ReadVertexData()
+    {
+        // defines which characters to split file into lines on:
+        var fileDelimiters = new[] { "\r\n", "\r", "\n" };
+
+        // defines which characters to split each line on:
+        var lineDelimiters = new[] { ' ' };
+
+        // split file into array of non-empty lines:
+        var lines = vertexFile.text.Split(fileDelimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        if (lines.Length < 1)
+        {
+            Debug.LogWarning($"{vertexFile.name} was empty!");
+            return;
+        }
+
+        var numVertices = int.Parse(lines[0]);
+
+        if (numVertices < 1)
+        {
+            Debug.LogWarning($"{vertexFile.name} contains no vertex data!");
+            return;
+        }
+
+        var vertices = new Vector3[numVertices];
+
+        for (var i = 1; i <= numVertices; i++)
+        {
+            // split line and read coordinates:
+            var elements = lines[i].Split(lineDelimiters, StringSplitOptions.RemoveEmptyEntries);
+            if (elements.Length < 3)
+            {
+                Debug.LogWarning($"{vertexFile.name} is missing data on line {i}");
+                continue;
+            }
+
+            vertices[i - 1] = new Vector3(
+                float.Parse(elements[0], CultureInfo.InvariantCulture),
+                float.Parse(elements[1], CultureInfo.InvariantCulture),
+                float.Parse(elements[2], CultureInfo.InvariantCulture)
+            );
+        }
+
+        // center mesh in world-space
+        for (var i = 0; i < vertices.Length; i++) vertices[i] -= offset;
+
+        _vertices = vertices;
+    }    
+}
