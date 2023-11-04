8ef5bb4395b113eecca16b4d51103dc47b1d3949
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-11-04 14:19:21 +0100

    Create meshBounds structure

---
+++ Assets/Scripts/MeshBounds.cs
@@ -0,0 +1,31 @@
+﻿// //////////////////////////////////////////////////////////////////////////
+// //////////////////////////////
+// //FileName: MeshBounds.cs
+// //FileType: Visual C# Source file
+// //Author : Anders P. Åsbø
+// //Created On : 04/11/2023
+// //Last Modified On : 04/11/2023
+// //Copy Rights : Anders P. Åsbø
+// //Description :
+// //////////////////////////////////////////////////////////////////////////
+// //////////////////////////////
+
+public struct MeshBounds
+{
+    public MeshBounds(float xMin, float xMax, float yMin, float yMax, float zMin, float zMax)
+    {
+        XMin = xMin; XMax = xMax;
+        YMin = yMin; YMax = yMax;
+        ZMin = zMin; ZMax = zMax;
+        Width = xMax - xMin;  Height = zMax - zMin;
+    }
+
+    public float XMin { get; }
+    public float XMax { get; }
+    public float YMin { get; }
+    public float YMax { get; }
+    public float ZMin { get; }
+    public float ZMax { get; }
+    public float Width { get; }
+    public float Height { get; }
+}
